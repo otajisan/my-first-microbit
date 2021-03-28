aa0f617680ea698c845ecfccc1b85fa4c9850e1e
Taji <taji.taji.otajisan@gmail.com>
2021-03-28 14:16:19 +0900

    feature: sing a random song

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+def on_received_number(receivedNumber):
+    basic.show_number(receivedNumber)
+radio.on_received_number(on_received_number)
+
+def on_first():
+    count_down()
+    sing(get_random_song())
+
 def count_down():
     for num in [3, 2, 1]:
         basic.show_number(num)
@@ -12,19 +20,44 @@ def smile():
     . # # # .
     """)
 
-def sing_kirakira_boshi():
-    basic.show_icon(IconNames.EIGTH_NOTE)
-    notes = "C - C - G - G - A - A - G G"
-    bpm = 180
-    music.play_melody(notes, bpm)
+def on_received_string(receivedString):
+    basic.show_string(receivedString)
+radio.on_received_string(on_received_string)
 
-def on_first():
-    count_down()
-    sing_kirakira_boshi()
+def kirakiraboshi():
+    return [
+        "C - C - G - G - A - A - G - - -",
+        "F - F - E - E - D - D - C - - -",
+        "G - G - F - F - E - E - D - - -",
+        "G - G - F - F - E - E - D - - -",
+        "C - C - G - G - A - A - G - - -",
+        "F - F - E - E - D - D - C - - -"
+    ].join(" ")
+
+def kaerunouta():
+    return [
+        "C - D - E - F - E - D - C - - -",
+        "E - F - G - A - G - F - E - - -",
+        "C - - - C - - - C - - - C - - -",
+        "C C D D E E F F E - D - C - - -"
+    ].join(" ")
+
+def get_random_song():
+    if Math.random_boolean():
+        return kaerunouta()
+    return kirakiraboshi()
+
+def sing(song):
+    basic.show_icon(IconNames.EIGTH_NOTE)
+    bpm = 240
+    music.play_melody(song, bpm)
+
+radio.set_group(1)
+radio.send_string(kirakiraboshi())
+
+on_first()
 
 def on_forever():
     smile()
     basic.clear_screen()
-
-on_first()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
+    basic.showNumber(receivedNumber)
+})
+function on_first() {
+    count_down()
+    sing(get_random_song())
+}
+
 function count_down() {
     for (let num of [3, 2, 1]) {
         basic.showNumber(num)
@@ -15,18 +23,33 @@ function smile() {
     `)
 }
 
-function sing_kirakira_boshi() {
+radio.onReceivedString(function on_received_string(receivedString: string) {
+    basic.showString(receivedString)
+})
+function kirakiraboshi(): string {
+    return ["C - C - G - G - A - A - G - - -", "F - F - E - E - D - D - C - - -", "G - G - F - F - E - E - D - - -", "G - G - F - F - E - E - D - - -", "C - C - G - G - A - A - G - - -", "F - F - E - E - D - D - C - - -"].join(" ")
+}
+
+function kaerunouta(): string {
+    return ["C - D - E - F - E - D - C - - -", "E - F - G - A - G - F - E - - -", "C - - - C - - - C - - - C - - -", "C C D D E E F F E - D - C - - -"].join(" ")
+}
+
+function get_random_song(): string {
+    if (Math.randomBoolean()) {
+        return kaerunouta()
+    }
+    
+    return kirakiraboshi()
+}
+
+function sing(song: string) {
     basic.showIcon(IconNames.EigthNote)
-    let notes = "C - C - G - G - A - A - G G"
-    let bpm = 180
-    music.playMelody(notes, bpm)
+    let bpm = 240
+    music.playMelody(song, bpm)
 }
 
-function on_first() {
-    count_down()
-    sing_kirakira_boshi()
-}
-
+radio.setGroup(1)
+radio.sendString(kirakiraboshi())
 on_first()
 basic.forever(function on_forever() {
     smile()
