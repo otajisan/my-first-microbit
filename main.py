836987aff0cb9c4dbea1990c04ec85ac6d3a3b18
Taji <taji.taji.otajisan@gmail.com>
2021-03-28 12:49:47 +0900

    add: note image for music

--- main.py
+++ main.py
@@ -1,15 +1,7 @@
-count_down()
-sing_kirakira_boshi()
-
 def count_down():
     for num in [3, 2, 1]:
         basic.show_number(num)
     basic.show_string("Go!")
-
-def sing_kirakira_boshi():
-    notes = "C - C - G - G - A - A - G G"
-    bpm = 180
-    music.play_melody(notes, bpm)
 
 def smile():
     basic.show_animation("""
@@ -20,8 +12,19 @@ def smile():
     . # # # .
     """)
 
+def sing_kirakira_boshi():
+    basic.show_icon(IconNames.EIGTH_NOTE)
+    notes = "C - C - G - G - A - A - G G"
+    bpm = 180
+    music.play_melody(notes, bpm)
+
+def on_first():
+    count_down()
+    sing_kirakira_boshi()
+
 def on_forever():
     smile()
     basic.clear_screen()
 
+on_first()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,16 +1,8 @@
-count_down()
-sing_kirakira_boshi()
 function count_down() {
     for (let num of [3, 2, 1]) {
         basic.showNumber(num)
     }
     basic.showString("Go!")
-}
-
-function sing_kirakira_boshi() {
-    let notes = "C - C - G - G - A - A - G G"
-    let bpm = 180
-    music.playMelody(notes, bpm)
 }
 
 function smile() {
@@ -23,6 +15,19 @@ function smile() {
     `)
 }
 
+function sing_kirakira_boshi() {
+    basic.showIcon(IconNames.EigthNote)
+    let notes = "C - C - G - G - A - A - G G"
+    let bpm = 180
+    music.playMelody(notes, bpm)
+}
+
+function on_first() {
+    count_down()
+    sing_kirakira_boshi()
+}
+
+on_first()
 basic.forever(function on_forever() {
     smile()
     basic.clearScreen()
